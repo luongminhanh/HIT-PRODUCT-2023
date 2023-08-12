03642bdcc78b7ea9d04ae72d067884e8b694a348
MinhAnh-haui <luongminhanh.haui@gmail.com>
2023-08-13 06:26:54 +0700

    fix order

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -61,118 +61,36 @@ const Order = () => {
     }, [])
 
     return (
-        <>
-            {
-                dataCustomer ?
-                    <div className='order order-container'>
-                        <div>
-                            <div className="order-infor">
-                                <div className="order-infor-user">
-                                    <div className='order-infor-user-title'>
-                                        <h3>THÔNG TIN NHẬN HÀNG</h3>
-                                    </div>
-                                    <div className="order-infor-user-form">
-                                        <div className="form-item">
-                                            <div>
-                                                <label htmlFor="">Họ tên người nhận <span>*</span></label> <br />
-                                                <input type="text" value={dataCustomer.fullName} />
-                                            </div>
-                                        </div>
-                                        <div className="form-item">
-                                            <div>
-                                                <label htmlFor="">Số điện thoại <span>*</span></label> <br />
-                                                <input type="text" value={dataCustomer.phoneNumber} />
-                                            </div>
-                                        </div>
-                                        <div className="form-item">
-                                            <div>
-                                                <label htmlFor="">Gmail <span>*</span></label> <br />
-                                                <input type="text" value={dataUser.email} />
-                                            </div>
-                                        </div>
-                                        <div className="form-item">
-                                            <div>
-                                                <label htmlFor="">Địa chỉ cụ thể <span>*</span></label> <br />
-                                                <input type="text" value={dataCustomer?.address?.addressName} />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="order-infor-products">
-                                    <div className='order-infor-products-title'>
-                                        <h3>SẢN PHẨM BẠN ĐẶT</h3>
-                                    </div>
-                                    <div className="order-infor-products-form">
-                                        <table>
-                                            <thead>
-                                                <tr>
-                                                    <th>Sản phẩm</th>
-                                                    <th>Tổng giá</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                {cart.cartItems && cart.cartItems.length > 0 && (
-                                                    <React.Fragment>
-                                                        {cart.cartItems.map((item) => (
-                                                            <tr key={item.productId}>
-                                                                <td className='products-name'>
-                                                                    {item.productName} <span className='attention'>x {item.quantity}</span>
-                                                                </td>
-                                                                <td>{(item.price * item.quantity).toLocaleString('vi-VN')}</td>
-                                                            </tr>
-                                                        ))}
-                                                    </React.Fragment>
-                                                )}
-                                                {/* <tr>
-                                    <td className='products-name'>
-                                        Bánh mì trứng <span className='attention'>x 5</span>
-                                    </td>
-                                    <td>
-                                        120.000.000VND
-                                    </td>
-                                </tr> */}
-                                            </tbody>
-                                        </table>
-                                    </div>
+        <div className='order order-container'>
+            <div>
+                <div className="order-infor">
+                    <div className="order-infor-user">
+                        <div className='order-infor-user-title'>
+                            <h3>THÔNG TIN NHẬN HÀNG</h3>
+                        </div>
+                        <div className="order-infor-user-form">
+                            <div className="form-item">
+                                <div>
+                                    <label htmlFor="">Họ tên người nhận <span>*</span></label> <br />
+                                    <input type="text" value={dataCustomer.fullName} />
                                 </div>
                             </div>
-                            <div className="order-pay">
-                                <div className="order-pay-title">
-                                    <h3>THANH TOÁN</h3>
+                            <div className="form-item">
+                                <div>
+                                    <label htmlFor="">Số điện thoại <span>*</span></label> <br />
+                                    <input type="text" value={dataCustomer.phoneNumber} />
                                 </div>
-                                <div className="order-pay-infor">
-                                    <table>
-                                        <thead>
-                                            <tr>
-                                                <th>Tổng tiền hàng:</th>
-                                                <td>{cart.cartTotalAmount.toLocaleString('vi-VN')}đ</td>
-                                            </tr>
-                                            <tr>
-                                                <th>Phí vận chuyển (tạm tính):</th>
-                                                <td>{cart.cartTotalAmount.toLocaleString('vi-VN') > 0 ? shipPrice.toLocaleString('vi-VN') : 0}đ</td>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td colSpan={2}>
-                                                    <hr />
-                                                </td>
-                                            </tr>
-                                            <tr style={{ fontSize: 20 }}>
-                                                <th>Tổng tiền: </th>
-                                                <th className='pay-money'>
-                                                    {cart.cartTotalAmount > 0 ? (cart.cartTotalAmount + shipPrice).toLocaleString('vi-VN') : 0}đ
-                                                </th>
-                                            </tr>
-                                            <tr>
-                                                <td colSpan={2}>
-                                                    <div>
-                                                        <Button text='MUA HÀNG' onClick={() => { handlePayBill(); navigate("/infor") }} />
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
+                            </div>
+                            <div className="form-item">
+                                <div>
+                                    <label htmlFor="">Gmail <span>*</span></label> <br />
+                                    <input type="text" value={dataUser.email} />
+                                </div>
+                            </div>
+                            <div className="form-item">
+                                <div>
+                                    <label htmlFor="">Địa chỉ cụ thể <span>*</span></label> <br />
+                                    <input type="text" value={dataCustomer?.address?.addressName} />
                                 </div>
                             </div>
                         </div>
@@ -202,14 +120,6 @@ const Order = () => {
                                             ))}
                                         </React.Fragment>
                                     )}
-                                    {/* <tr>
-                                        <td className='products-name'>
-                                            Bánh mì trứng <span className='attention'>x 5</span>
-                                        </td>
-                                        <td>
-                                            120.000.000VND
-                                        </td>
-                                    </tr> */}
                                 </tbody>
                             </table>
                         </div>
